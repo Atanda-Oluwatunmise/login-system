fd80e2a17334a48a646523c64897827c555e428b
Atanda-Oluwatunmise <75289869+Atanda-Oluwatunmise@users.noreply.github.com>
2021-03-08 12:52:10 +0100

    Update form.php

--- form.php
+++ form.php
@@ -1,9 +1,10 @@
 <!DOCTYPE HTML>  
 <html>
 <head>
-<meta charset="utf-8">
-		<title>Login</title>
-		<link rel="stylesheet" href="style.css" type = "text/css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>Login</title>
+	<link rel="stylesheet" href="style.css" type = "text/css">
 </head>
 <body> 
 <?php session_start();
